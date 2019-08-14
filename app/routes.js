2b2d4b1cb6edecc79e7807e53c9e0cb3134d79db
Ralph Hawkins <ralph.hawkins@beis.gov.uk>
2019-08-14 10:03:16 +0100

    first commit

---
+++ app/routes.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const router = express.Router()
+
+// Add your routes here - above the module.exports line
+
+module.exports = router
+
+router.post('/notification-flow/ingredients-document-upload', function (req, res) {
+  // Get the answer from session data
+  // The name between the quotes is the same as the 'name' attribute on the input elements
+  // However in JavaScript we can't use hyphens in variable names
+
+  let formulationType = req.session.data['formulation-type']
+
+  if (formulationType === 'choose-a-predefined-frame-formulation') {
+    res.redirect('/notification-flow/ingredients-of-concern-check')
+  } else {
+    res.redirect('/notification-flow/ingredients-document-upload')
+  }
+})
+
+
+router.post('/notification-flow/ingredients-of-concern-document-upload', function (req,res) {
+
+  let harmfulIngredients = req.session.data['does-the-product-contain-any-ingredients-of-concern']
+
+  if (harmfulIngredients === 'yes') {
+    res.redirect('/notification-flow/ingredients-of-concern-document-upload')
+  } else {
+    res.redirect('/notification-flow/ph-between-3-and-10')
+  }
+
+})
